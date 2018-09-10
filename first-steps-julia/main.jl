030804eee7ace00b8e26de590be0eb664862f578
Michael Shen <mishen@umich.edu>
2018-09-10 17:01:32 -0400

    9/10/18 changes

--- first-steps-julia/main.jl
+++ first-steps-julia/main.jl
@@ -1,1 +1,31 @@
-Pkg.add("Images")
+using Pkg;
+Pkg.add("Images");
+Pkg.add("DataFrames");
+Pkg.add("CSV");
+Pkg.add("StatsBase");
+Pkg.add("Plots");
+Pkg.add("StatPlots");
+using Images;
+using DataFrames;
+using CSV;
+using StatsBase;
+using Plots;
+using StatPlots;
+
+gr()
+
+#Path to files and folders
+path = "/Users/mishen/Documents/GitHub/julia/first-steps-julia"
+
+#Read training data information
+trainData = CSV.read("$(path)/trainlabels.csv");
+
+# println(trainData)
+# println(trainData[:Class])
+
+#Count occurence of each character in training data
+trainDataCounts = by(trainData, :Class, nrow);
+trainDataCountsSort = sort(trainDataCounts, order(:x1))
+
+# plot(trainDataCountsSort[:Row], trainDataCountsSort[:x1])
+@df trainDataCountsSort plot(:Class, :x1)
